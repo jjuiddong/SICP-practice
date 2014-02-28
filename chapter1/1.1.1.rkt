f72944601249ef24b584c58486bca2ad5a21aa14
jjuiddong <jjuiddong@gmail.com>
2014-02-28 21:36:41 +0900

    1.1.1

---
+++ chapter1/1.1.1.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname 1.1.1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
+486
+
+(+ 137 349)
+
+(- 1000 334)
+
+(+ 2.7 10)
