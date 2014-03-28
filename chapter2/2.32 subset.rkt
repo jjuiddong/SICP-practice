f21a6074828035b84cedf6dac9ec3ebcbf9478de
jjuiddong <jjuiddong@hanmail.net>
2014-03-29 00:58:31 +0900

    update 2.32

--- chapter2/2.32 subset.rkt
+++ chapter2/2.32 subset.rkt
@@ -1,3 +1,12 @@
 ;problem 2.32
 ;subset
 
+(define (subsets s)
+  (if (null? s)
+      (list null)
+      (let ((rest (subsets (cdr s))))
+        (append rest (map 
+                      (lambda (x) (cons (car s) x)) 
+                      rest)))))
+
+(subsets (list 1 2 3))
