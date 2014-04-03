1bde100a740769fd196c1460572591cfc6ce7cdc
jjuiddong <jjuiddong@hanmail.net>
2014-04-03 17:53:38 +0900

    2.2.2 permutation

--- chapter2/global.rkt
+++ chapter2/global.rkt
@@ -92,5 +92,38 @@
 
 
 
+
+; problem 1.21
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (smallest-divisor2 n)
+  (find-divisor2 n 2))
+
+; problem 1.23
+(define (find-divisor2 n test-divisor)
+  (define (next n)
+     (cond ((= n 2) 3)
+           (else (+ n 2))))   
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor2 n (next test-divisor)))))
+
+ 
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (prime2? n)
+  (= n (smallest-divisor2 n)))
+
+
 ; export
 (provide (all-defined-out))
