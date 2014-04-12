94dd213fd93c0015ff567e34252202c7f09c14da
jjuiddong <jjuiddong@gmail.com>
2014-04-12 19:01:45 +0900

    2.53

---
+++ chapter2/2.53 quote.rkt
@@ -0,0 +1,9 @@
+;problem 2.53
+;quote
+
+(list 'a 'b 'c)
+(list (list 'george))
+(cdr '((x1 x2) (y1 y2)))
+(pair? (car '(a short list)))
+(memq 'red '((red shoes) (blue socks)))
+(memq 'red '(red shoes blue socks))
