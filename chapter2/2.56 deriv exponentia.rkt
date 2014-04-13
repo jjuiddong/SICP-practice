73483d8790ee6c2549694d8d23ee943b8b62f0af
jjuiddong <jjuiddong@gmail.com>
2014-04-13 10:16:47 +0900

    2.56

---
+++ chapter2/2.56 deriv exponentia.rkt
@@ -0,0 +1,58 @@
+;problem 2.56
+;deriv exponentia
+
+(define (variable? x) (symbol? x))
+
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (make-sum a1 a2) 
+  (cond ((and (number? a1) (number? a2)) (+ a1 a2))
+        ((=number? a1 0) a2)
+        ((=number? a2 0) a1)        
+        (else (list '+ a1 a2))))
+
+(define (make-product m1 m2) 
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        (else (list '* m1 m2))))
+
+(define (sum? x)
+  (and (pair? x) (eq? (car x) '+)))
+
+(define (product? x)
+  (and (pair? x) (eq? (car x) '*)))
+
+(define (augend s) (cadr s))
+
+(define (addend s) (caddr s))
+
+(define (multiplicand p) (cadr p))
+
+(define (multiplier p) (caddr p))
+
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+          (make-product (multiplier exp)
+                        (deriv (multiplicand exp) var))
+          (make-product (deriv (multiplier exp) var)
+                        (multiplicand exp))))
+        (else
+         (error "unknown expression type -- DERIVE" exp))))
+
+
+(deriv '(+ x 3) 'x)
+(deriv '(* x y) 'x)
+(deriv '(* (* x y) (+ x 3)) 'x)
